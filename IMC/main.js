2eb4ac55b41e11cb139b1dae9cd7fd835fe0c835
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-07-09 22:54:29 -0300

    Add main.js with initial variables and IMC calculation

---
+++ IMC/main.js
@@ -0,0 +1,12 @@
+const name = 'Rodrigo de Lima'
+const surname = 'Silva'
+const age = 44
+const weight = 95
+const height = 1.72
+let IMC = 30
+
+total = (weight / height) ** 2
+
+console.log(name, surname, "has", age, "years and", weight, "of weight and", height, "of height")
+console.log("The", name, surname, "has", total, "IMC")
+
